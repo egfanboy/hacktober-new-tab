5f557ffa468d25dd65378f8386c90c5ddfd1c98d
Palak Chawla <chawla.palak19@gmail.com>
2020-10-26 21:24:35 -0400

    add hacktoberfest page button

---
+++ extension/src/components/button-to/Button.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="example-2">
+        <button v-on:click="greet">Hacktoberfest</button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Button',
+    data: function() {
+        return {
+            myModel: false,
+        };
+    },
+    methods: {
+        greet: function() {
+            window.open('https://hacktoberfest.digitalocean.com/', '_blank');
+        },
+    },
+};
+</script>

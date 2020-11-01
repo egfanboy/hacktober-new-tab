652504213ebf64d1c189b371b84e16ec1d3bcfe7
Palak Chawla <chawla.palak19@gmail.com>
2020-10-31 23:44:27 -0400

    add random background image and credits on load

---
+++ extension/src/data/constants.js
@@ -0,0 +1,5 @@
+const API_KEY = '4aNPYjKERA2Rp4GCVLKacX72bfvoCAGQpB74mNjp15I';
+const ORIENTATION = 'landscape';
+const PHOTO_ENDPOINT = `https://api.unsplash.com/photos/random/?client_id=${API_KEY}&orientation=${ORIENTATION}`;
+
+export default PHOTO_ENDPOINT;
